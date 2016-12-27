71ad6b5f5af0cf3fdf01f13abee12090be1efaa3
Arsen Terletsky <aaarsennn@bigmir.net>
2016-12-27 12:28:00 +0200

    test commit

--- client/imports/app/accounts/accounts-module.ts
+++ client/imports/app/accounts/accounts-module.ts
@@ -2,7 +2,7 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {FormsModule} from '@angular/forms';
 import {LoginComponent} from './login.component';
-import {AuthGuard} from "./annotations";
+import {AuthGuard} from './annotations';
 import { Authorization } from './authorization';
 
 @NgModule({
