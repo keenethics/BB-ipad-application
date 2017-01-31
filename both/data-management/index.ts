6d5203663df773b50010c3ee84a24df7a585bf35
Arsen Terletsky <aaarsennn@bigmir.net>
2017-01-31 10:21:47 +0200

    cleaning the project

--- both/data-management/index.ts
+++ both/data-management/index.ts
@@ -1,3 +1,2 @@
 export { BusinessData, BusinessDataUnit } from './business-data.collection';
 export { ColumnNames, ColumnNamesCollection } from './column-names.collection';
-export { toOverviewTable } from './data-aggregation';
--- server/main.ts
+++ server/main.ts
@@ -1,7 +1,4 @@
 import { Meteor } from 'meteor/meteor';
-import { loadD3Map } from './imports/fixtures/d3map.fixtures';
-
-import './imports/methods/d3map.methods';
 
 import './imports/authorization';
 import './imports/data-management';
@@ -10,5 +7,4 @@ Meteor.startup(() => {
   Accounts.config({
     forbidClientAccountCreation: false
   });
-  loadD3Map();
 });
