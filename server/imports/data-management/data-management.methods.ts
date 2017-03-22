2c57ebafddeabd54ae706ec3a0feffccffdec43d
aarsenn <aaarsennn@gigmir.net>
2017-03-22 18:52:15 +0200

    fix data upload

--- server/imports/data-management/data-management.methods.ts
+++ server/imports/data-management/data-management.methods.ts
@@ -19,6 +19,7 @@ import {
 } from '../../../both/data-management';
 
 declare const process: any;
+declare const __dirname: string;
 
 export const uploadFile = new ValidatedMethod({
   name: 'data.upload',
@@ -38,38 +39,43 @@ export const uploadFile = new ValidatedMethod({
       throw new Meteor.Error('no coordinates', 'no_coordinates');
     }
 
+    const rootPath = (Meteor as any).rootPath;
     const absoluteFilePath = Assets.absoluteFilePath('save-data.js');
+    const assetsPath = absoluteFilePath.substring(0, absoluteFilePath.indexOf('save-data.js'));
+    const babyparseLinkCommand = `${rootPath}/npm/node_modules/babyparse ${assetsPath}node_modules/babyparse`;
+    const mongodbLinkCommand = `${rootPath}/npm/node_modules/mongodb ${assetsPath}node_modules/mongodb`;
     const tempFileURI = `${absoluteFilePath.substring(0, absoluteFilePath.indexOf('save-data.js'))}temp`;
-
     const mongoUrl = process.env.MONGO_URL;
 
     if (fs.existsSync(tempFileURI)) throw new Meteor.Error('data uploading in process', 'data_calculation_in_process');
 
-    fs.writeFile(tempFileURI, fileData, function (err: any) {
-      if (err) throw new Meteor.Error(err.message, err.message);
-      console.log('Data saved to the temp file.');
-      console.time();
-
-      exec(`node ${absoluteFilePath} ${mongoUrl}`, function (err: any, stdout: any, strerr: any) {
+    exec(`ln -s -f ${babyparseLinkCommand} && ln -s -f ${mongodbLinkCommand}`, () => {
+      fs.writeFile(tempFileURI, fileData, function (err: any) {
         if (err) throw new Meteor.Error(err.message, err.message);
-        Fiber(() => {
-          const titles = (BusinessData as any)
-            .aggregate([{ $group: { _id: null, titles: { $addToSet: '$n2' } } }])[0]
-            .titles as string[];
-          UnitsTitles.remove({});
-          titles.forEach(t => UnitsTitles.insert({ title: t }));
+        console.log('Data saved to the temp file.');
+        console.time();
 
-          MarketCountries.remove({});
-          AvailableCountries.remove({});
+        exec(`node ${absoluteFilePath} ${mongoUrl}`, function (err: any, stdout: any, strerr: any) {
+          if (err) throw new Meteor.Error(err.message, err.message);
+          Fiber(() => {
+            const titles = (BusinessData as any)
+              .aggregate([{ $group: { _id: null, titles: { $addToSet: '$n2' } } }])[0]
+              .titles as string[];
+            UnitsTitles.remove({});
+            titles.forEach(t => UnitsTitles.insert({ title: t }));
 
-          setMarketCountries();
-          setAvailableCountries();
+            MarketCountries.remove({});
+            AvailableCountries.remove({});
 
-          console.log('Data uploaded');
-          console.timeEnd();
+            setMarketCountries();
+            setAvailableCountries();
 
-          DataUpdates.update({}, { lastDataUpdateDate: new Date() }, { upsert: true });
-        }).run();
+            console.log('Data uploaded');
+            console.timeEnd();
+
+            DataUpdates.update({}, { lastDataUpdateDate: new Date() }, { upsert: true });
+          }).run();
+        });
       });
     });
 
