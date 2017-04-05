83a99d163266a8d235086a7d12a444633f6fb8ec
aarsenn <aaarsennn@gigmir.net>
2017-04-05 11:45:39 +0300

    filter couter

--- client/imports/app/filters/filter-controller.ts
+++ client/imports/app/filters/filter-controller.ts
@@ -6,7 +6,8 @@ import { AvailableCountries } from '../../../../both/countries/available-countri
 
 @Injectable()
 export class FilterController {
-  private currentFilter = new BehaviorSubject(this.getFromStorage());
+  private currentFilter: BehaviorSubject<any>;
+  private selectedOptions: BehaviorSubject<any[]>;
   private availableCountries: string[] = [];
   public onChangeCategory = new EventEmitter();
   public onSelectCountry = new EventEmitter();
@@ -14,6 +15,10 @@ export class FilterController {
   public onInitFilters = new EventEmitter();
 
   constructor() {
+    const { mapQueryObject, activeFilters } = this.getFromStorage();
+    this.currentFilter = new BehaviorSubject(mapQueryObject);
+    this.selectedOptions = new BehaviorSubject(activeFilters);
+
     MeteorObservable.subscribe('available-countries').subscribe(() => {
       this.availableCountries = AvailableCountries.find({}).fetch().map((c: any) => c.name);
     });
@@ -31,6 +36,20 @@ export class FilterController {
     } catch (err) {
       this.onInitFilters.emit();
     }
+  }
+
+  set activeFilters$(f: any) {
+    try {
+      const { category, mapQueryObject, filterQueryObject } = this.getFromStorage();
+      this.saveToStorage(category, f, filterQueryObject, mapQueryObject);
+    } catch (err) {
+      this.onInitFilters.emit();
+    }
+    this.selectedOptions.next(f);
+  }
+
+  get activeFilters$() {
+    return this.selectedOptions.asObservable();
   }
 
   initFilters() {
