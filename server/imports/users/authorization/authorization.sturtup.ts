19989fc9c1b5f32a2ce0b73ee6e0c2d9fee0e00e
Arsen <aaarsennn@bigmir.net>
2017-06-07 13:15:15 +0300

    user logins

--- server/imports/users/authorization/authorization.sturtup.ts
+++ server/imports/users/authorization/authorization.sturtup.ts
@@ -2,6 +2,8 @@ import { Meteor } from 'meteor/meteor';
 import { Accounts } from 'meteor/accounts-base';
 
 Meteor.startup(() => {
+  registerLoginHooks();
+
   if (Meteor.users.find().count() === 0) {
     const userId = Accounts.createUser({
       username: 'admin',
@@ -12,3 +14,28 @@ Meteor.startup(() => {
     Roles.addUsersToRoles(userId, 'Administrator');
   }
 });
+
+function registerLoginHooks() {
+  Accounts.onLogin(() => {
+    incUserLogins(Meteor.userId());
+  });
+
+  Accounts.onLoginFailure((err: any) => {
+    incUserFailedLogins(err.user._id);
+  });
+}
+
+
+function incUserLogins(userId: string) {
+  Meteor.users.update(userId, {
+    $inc: { 'profile.logins': 1 },
+    $currentDate: { 'profile.lastLoginDate': true }
+  });
+}
+
+function incUserFailedLogins(userId: string) {
+  Meteor.users.update(userId, {
+    $inc: { 'profile.failedLogins': 1 },
+    $currentDate: { 'profile.lastFailedLoginDate': true }
+  });
+}
